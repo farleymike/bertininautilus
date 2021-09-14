af63912ed787c2cd45433f025e4f55ceb662ef6c
Mike Farley <mfarley@sdsu.edu>
2021-09-14 07:44:01 -0700

    Removed MPI testing support.

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,6 @@ RUN apk --no-cache add curl
 RUN apk add --no-cache \
         python3 \
         py3-pip \
-        openssh \
-        openmpi \
     && pip3 install --upgrade pip \
     && pip3 install \
         awscli \
--- runbertini.sh
+++ runbertini.sh
@@ -13,7 +13,7 @@ echo "S3 download complete..."
 
 echo "Running Bertini..."
 
-mpirun -np 2 --allow-run-as-root /bertini/BertiniLinux64_v1.6/bertini /bertini/files/$2
+/bertini/BertiniLinux64_v1.6/bertini /bertini/files/$2
 
 echo "Bertini has completed..."
 
